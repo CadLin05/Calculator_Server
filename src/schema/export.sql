a3faf95862dbfe5acf3e2c155db869456e144e7a
CadLin05 <157499066+CadLin05@users.noreply.github.com>
2026-02-24 21:59:40 -0500

    Add 'answer' column to export.sql schema

--- src/schema/export.sql
+++ src/schema/export.sql
@@ -13,6 +13,7 @@ CREATE TABLE `calculation` (
   `val1` INT NOT NULL,
   `val2` INT NOT NULL,
   `operator` varchar(45) NOT NULL,
+  `answer` INT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
